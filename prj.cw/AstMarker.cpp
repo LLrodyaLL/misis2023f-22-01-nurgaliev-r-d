65c977bfb9172994255056d09448069525f19f6a
LLrodyaLL <113860484+LLrodyaLL@users.noreply.github.com>
2023-11-27 10:34:57 +0300

    Update AstMarker.cpp

--- prj.cw/AstMarker.cpp
+++ prj.cw/AstMarker.cpp
@@ -1,21 +1,19 @@
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
-#include<imgui_impl_opengl3_loader.h>
+#include "imgui_impl_opengl3_loader.h"
 #include <stdio.h>
 #include <string>
 #include "nfd.h"
+#include <GL/GL.h>
+#include <opencv2/opencv.hpp>
 #define GL_SILENCE_DEPRECATION
 #if defined(IMGUI_IMPL_OPENGL_ES2)
 #include <GLES2/gl2.h>
 #endif
-#include <GLFW/glfw3.h> // Will drag system OpenGL headers
+#include <GLFW/glfw3.h>
 
-#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
-#pragma comment(lib, "legacy_stdio_definitions")
-#endif
-
-#ifdef __EMSCRIPTEN__   
+#ifdef EMSCRIPTEN
 #include "../libs/emscripten/emscripten_mainloop_stub.h"
 #endif
 
@@ -24,12 +22,61 @@ static void glfw_error_callback(int error, const char* description)
     fprintf(stderr, "GLFW Error %d: %s\n", error, description);
 }
 
-
-
 bool showButtons = false;
 bool showSubButtons = false;
 
-// Main code
+std::string exportImagePath;
+cv::Mat exportImage;
+
+// Функция для загрузки изображения по указанному пути
+void LoadExportImage(const std::string& imagePath) {
+    exportImage = cv::imread(imagePath, cv::IMREAD_UNCHANGED);
+    if (exportImage.empty()) {
+        // Вывести информацию об ошибке загрузки
+        std::cerr << "Error loading image from path: " << imagePath << std::endl;
+        const char* errorMessage = "Unable to load image. Check if the file exists and is a valid image format.";
+        ImGui::OpenPopup("Error");
+        ImGui::SetNextWindowSize(ImVec2(400, 100));
+        if (ImGui::BeginPopupModal("Error", NULL, ImGuiWindowFlags_AlwaysAutoResize)) {
+            ImGui::Text("%s", errorMessage);
+            if (ImGui::Button("OK", ImVec2(120, 0))) {
+                ImGui::CloseCurrentPopup();
+            }
+            ImGui::EndPopup();
+        }
+    }
+    else {
+        exportImagePath = imagePath;
+        // Вывести информацию об успешной загрузке
+        std::cout << "Image loaded successfully from path: " << imagePath << std::endl;
+    }
+}
+
+// Функция для обработки нажатия кнопки "Export"
+void HandleExportButton() {
+    nfdchar_t* outPath = NULL;
+    nfdresult_t result = NFD_SaveDialog("jpg,png", NULL, &outPath);
+
+    if (result == NFD_OKAY) {
+        if (!exportImage.empty()) {
+            cv::imread(outPath);
+            LoadExportImage(outPath);
+        }
+        else {
+            puts("Trying to save an empty Image.");
+        }
+        free(outPath);
+    }
+    else if (result == NFD_CANCEL) {
+        puts("User pressed cancel.");
+        free(outPath);
+    }
+    else {
+        printf("Error: %s\n", NFD_GetError());
+        free(outPath);
+    }
+}
+
 int main(int, char**)
 {
     glfwSetErrorCallback(glfw_error_callback);
@@ -53,16 +100,19 @@ int main(int, char**)
 #else
     // GL 3.0 + GLSL 130
     const char* glsl_version = "#version 130";
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
     //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
 #endif
 
+    GLFWmonitor* primary = glfwGetPrimaryMonitor();
+    const GLFWvidmode* mode = glfwGetVideoMode(primary);
+    glfwWindowHint(GLFW_DECORATED, GLFW_TRUE);
     // Create window with graphics context
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "AstMarker", nullptr, nullptr);
-    if (window == nullptr)
+    GLFWwindow* window = glfwCreateWindow(mode->width, mode->height, "AstMarker", nullptr, nullptr);
+    if (!window) {
+        glfwTerminate();
         return 1;
+    }
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1); // Enable vsync
 
@@ -81,23 +131,18 @@ int main(int, char**)
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init(glsl_version);
 
-
     // Our state
     bool show_demo_window = true;
     bool show_another_window = false;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
-    // Main loop
-#ifdef __EMSCRIPTEN__
-    // For an Emscripten build we are disabling file-system access, so let's not attempt to do a fopen() of the imgui.ini file.
-    // You may manually call LoadIniSettingsFromMemory() to load settings from your own storage.
+#ifdef EMSCRIPTEN
     io.IniFilename = nullptr;
     EMSCRIPTEN_MAINLOOP_BEGIN
 #else
     while (!glfwWindowShouldClose(window))
 #endif
     {
-
         glfwPollEvents();
 
         // Start the Dear ImGui frame
@@ -105,14 +150,43 @@ int main(int, char**)
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        ImGuiIO& io = ImGui::GetIO();
-        float windowWidth = io.DisplaySize.x;
-        float windowHeight = io.DisplaySize.y;
-
-
         {
-            static float f = 0.0f;
-            static int counter = 0;
+            ImGui::SetNextWindowPos(ImVec2(0, 0));
+            ImGui::Begin("Settings");
+
+            if (ImGui::Button("Files", ImVec2(160, 30))) {
+                showButtons = !showButtons;
+            }
+
+            if (showButtons) {
+                if (ImGui::Button("Import a set of points", ImVec2(160, 30))) {
+                    nfdchar_t* outPath = NULL;
+                    nfdresult_t result = NFD_OpenDialog(NULL, NULL, &outPath);
+
+
+                    if (result == NFD_OKAY) {
+                        puts("Success!");
+                        puts(outPath);
+                        free(outPath);
+                    }
+                    else if (result == NFD_CANCEL) {
+                        puts("User pressed cancel.");
+                    }
+                    else {
+                        printf("Error: %s\n", NFD_GetError());
+                    }
+                }
+
+                if (ImGui::Button("Export", ImVec2(160, 30))) {
+                    HandleExportButton();
+                }
+
+                if (ImGui::Button("Close", ImVec2(160, 30))) {
+                    showButtons = false;
+                }
+            }
+
+            ImGui::End();
 
             ImGui::SetNextWindowPos(ImVec2(100, 300));
             ImGui::SetNextWindowSize(ImVec2(500, 500));
@@ -130,9 +204,14 @@ int main(int, char**)
             float posX_1 = windowPos_1.x + (windowSize_1.x - textSize_1.x) * 0.5f;
             float posY_1 = windowPos_1.y + (windowSize_1.y - textSize_1.y) * 0.5f;
 
-            ImDrawList* drawList = ImGui::GetWindowDrawList();
-            drawList->AddText(ImVec2(posX_1, posY_1), ImGui::GetColorU32(ImGuiCol_Text), "Window 1");
-
+            if (!exportImagePath.empty() && !exportImage.empty()) {
+                GLuint textureID = 0;
+                glGenTextures(1, &textureID);
+                glBindTexture(GL_TEXTURE_2D, textureID);
+                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, exportImage.cols, exportImage.rows, 0, GL_RGBA, GL_UNSIGNED_BYTE, exportImage.ptr());
+                ImGui::Image((void*)(intptr_t)textureID, ImVec2(exportImage.cols, exportImage.rows));
+                glDeleteTextures(1, &textureID);
+            }
 
             ImGui::End();
             ImGui::PopStyleVar();
@@ -157,8 +236,6 @@ int main(int, char**)
             ImDrawList* drawList_2 = ImGui::GetWindowDrawList();
             drawList_2->AddText(ImVec2(posX_2, posY_2), ImGui::GetColorU32(ImGuiCol_Text), "Window 2");
 
-           
-
             ImGui::End();
             ImGui::PopStyleVar();
             ImGui::PopStyleColor();
@@ -171,11 +248,10 @@ int main(int, char**)
             ImGui::Begin("OVERLAY WINDOW");
 
             ImGuiWindowFlags_NoScrollbar;
-            
+
             ImVec2 windowSize_3 = ImGui::GetWindowSize();
             ImVec2 windowPos_3 = ImGui::GetWindowPos();
             ImVec2 textSize_3 = ImGui::CalcTextSize("Overlay Window");
-
             float posX_3 = windowPos_3.x + (windowSize_3.x - textSize_3.x) * 0.5f;
             float posY_3 = windowPos_3.y + (windowSize_3.y - textSize_3.y) * 0.5f;
 
@@ -185,73 +261,22 @@ int main(int, char**)
             ImGui::End();
             ImGui::PopStyleVar();
             ImGui::PopStyleColor();
-
-
-
-            ImGui::SetNextWindowPos(ImVec2(0, 0));
-            ImGui::Begin("Settings");
-            ImGuiWindowFlags_None;
-
-            if (ImGui::Button("Files", ImVec2(160, 30))) {
-                showButtons = !showButtons;
-            }
-
-            if (showButtons) {
-                if (ImGui::Button("Import a set of points", ImVec2(160, 30))) {
-                    nfdchar_t* outPath = NULL;
-                    nfdresult_t result = NFD_OpenDialog(NULL, NULL, &outPath);
-
-                    if (result == NFD_OKAY) {
-                        puts("Success!");
-                        puts(outPath);
-                        free(outPath);
-                    }
-                    else if (result == NFD_CANCEL) {
-                        puts("User pressed cancel.");
-                    }
-                    else {
-                        printf("Error: %s\n", NFD_GetError());
-                    }
-                }
-
-                if (ImGui::Button("Export", ImVec2(160, 30))) {
-                    nfdchar_t* outPath = NULL;
-                    nfdresult_t result = NFD_OpenDialog(NULL, NULL, &outPath);
-
-                    if (result == NFD_OKAY) {
-                        puts("Success!");
-                        puts(outPath);
-                        free(outPath);
-                    }
-                    else if (result == NFD_CANCEL) {
-                        puts("User pressed cancel.");
-                    }
-                    else {
-                        printf("Error: %s\n", NFD_GetError());
-                    }
-                }
-
-                if (ImGui::Button("Close", ImVec2(160, 30))) {
-                    showButtons = false;
-                }
-            }
-
-            ImGui::End();
-
         }
+
 
         // Rendering
         ImGui::Render();
         int display_w, display_h;
         glfwGetFramebufferSize(window, &display_w, &display_h);
         glViewport(0, 0, display_w, display_h);
-        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+        glClearColor(clear_color.x* clear_color.w, clear_color.y* clear_color.w, clear_color.z* clear_color.w, clear_color.w);
         glClear(GL_COLOR_BUFFER_BIT);
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
         glfwSwapBuffers(window);
     }
-#ifdef __EMSCRIPTEN__
+
+#ifdef EMSCRIPTEN
     EMSCRIPTEN_MAINLOOP_END;
 #endif
 
@@ -265,3 +290,4 @@ int main(int, char**)
 
     return 0;
 }
+
