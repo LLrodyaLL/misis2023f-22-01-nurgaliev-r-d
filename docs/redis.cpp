0b6afc45b48b00a9529864712886312310a5d18a
LLrodyaLL <113860484+LLrodyaLL@users.noreply.github.com>
2023-12-26 00:01:30 +0300

    Update redis.cpp

--- docs/redis.cpp
+++ docs/redis.cpp
@@ -1,1 +1,20 @@
+#include <cpp_redis/cpp_redis>
+
+int main() {
+    cpp_redis::client client("localhost", 6379);
+
+    // Чтение данных
+    auto reply = client.get("ключ");
+    if (reply.is_string()) {
+        std::cout << "Значение: " << reply.as_string() << std::endl;
+    }
+
+    // Запись данных (put, аналог set)
+    client.set("ключ", "значение");
+
+    // Удаление данных
+    client.del("ключ");
+
+    return 0;
+}
 
