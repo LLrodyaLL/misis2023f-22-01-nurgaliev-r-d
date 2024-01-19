db0bb7557eda1a5b3e7de60c4a997866397476ad
LLrodyaLL <113860484+LLrodyaLL@users.noreply.github.com>
2024-01-19 22:55:20 +0300

    Update image.h

--- prj.cw/image.h
+++ prj.cw/image.h
@@ -29,19 +29,6 @@ public:
      */
     void openImage();
 
-
-    sf::RectangleShape cursor1_1;  /**< Прямоугольник 1_1 для создания 1-го курсора. */
-    sf::RectangleShape cursor1_2;  /**< Прямоугольник 1_2 для создания 1-го курсора. */
-    sf::RectangleShape cursor1_3;  /**< Прямоугольник 1_3 для создания 2-го курсора. */
-    sf::RectangleShape cursor1_4;  /**< Прямоугольник 1_4 для создания 2-го курсора. */
-    sf::RectangleShape cursor2_1;  /**< Прямоугольник 2_1 для создания 3-го курсора. */
-    sf::RectangleShape cursor2_2;  /**< Прямоугольник 2_2 для создания 3-го курсора. */
-    sf::RectangleShape cursor2_3;  /**< Прямоугольник 2_3 для создания 4-го курсора. */
-    sf::RectangleShape cursor2_4;  /**< Прямоугольник 2_4 для создания 4-го курсора. */
-    sf::RectangleShape cursor3_1;  /**< Прямоугольник 3_1 для создания 5-го курсора. */
-    sf::RectangleShape cursor3_2;  /**< Прямоугольник 3_2 для создания 5-го курсора. */
-    sf::RectangleShape cursor3_3;  /**< Прямоугольник 3_3 для создания 6-го курсора. */
-    sf::RectangleShape cursor3_4;  /**< Прямоугольник 3_4 для создания 6-го курсора. */
 };
 
 #endif //ASTMARKER_IMAGE_H
